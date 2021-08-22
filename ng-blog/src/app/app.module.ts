bb028118eeaf1ff92facf56f63c2db93825b330b
prince1998 <priyanshramnani21@gmail.com>
2021-08-22 14:28:26 +0530

    Error

---
+++ ng-blog/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { environment } from 'src/environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestore,
+    AngularFireAuth,
+    AngularFireStorageModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ng-blog/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyBHwcKUiZJcn0tWQC7pPXe5zff3Ruq-qWE",
+  authDomain: "ng-blog-c1ebe.firebaseapp.com",
+  projectId: "ng-blog-c1ebe",
+  storageBucket: "ng-blog-c1ebe.appspot.com",
+  messagingSenderId: "889753709309",
+  appId: "1:889753709309:web:81d596517bb9ba78daec94",
+  measurementId: "G-KWTJHXZD54"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
